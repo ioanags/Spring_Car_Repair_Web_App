15e2f4f2c3bdf1e37c6da8b1a659dd07dfc76255
Giannis <giannhsags@gmail.com>
2018-12-10 19:03:44 +0200

    added-repair-and-person-services

---
+++ src/main/java/com/carshoprepair/carshop/controller/PersonController.java
@@ -0,0 +1,35 @@
+package com.carshoprepair.carshop.controller;
+
+import com.carshoprepair.carshop.domain.Person;
+import com.carshoprepair.carshop.domain.Repair;
+import com.carshoprepair.carshop.service.PersonServiceImpl;
+import com.carshoprepair.carshop.service.RepairServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class PersonController {
+    @Autowired
+    private PersonServiceImpl personService;
+    @Autowired
+    private RepairServiceImpl repairService;
+    @GetMapping("/admin")
+    public String index() {
+        return "admin_form";
+    }
+    @GetMapping("/hello")
+
+    public String hello(Model model, @RequestParam(value = "id") long id) {
+       Optional<Person> user = personService.getPersonById(id);
+            user.ifPresent(person ->model.addAttribute("name",person.getFirstName()));
+            user.ifPresent(person ->model.addAttribute("surname",person.getLastName()));
+            return "hello";
+
+    }
+}
